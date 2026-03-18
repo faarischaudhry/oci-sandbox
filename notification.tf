f52f467eb687634fa8b977c4d9815148a0f682b6
AB Paxtor Garcia <64762646+paxabacus@users.noreply.github.com>
2026-03-18 13:03:06 -0500

    Update notification.tf
    file change

--- notification.tf
+++ notification.tf
@@ -1,1 +1,6 @@
 # this be where notifications are
+resource "oci_ons_notification_topic" "test_notification_topic" {
+    #Required
+    compartment_id = ocid1.compartment.oc1..aaaaaaaa2ipyqhgat7gbkiqsqniivd4smro7n4xh4qfgq7hu5phwtjpij44q
+    name = "test_notification_id"
+}
