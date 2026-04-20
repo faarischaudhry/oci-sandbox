7d8825197fcda94dea6adce2b8c46ca9b6fe702b
faarischaudhry <faaris.chaudhry@oracle.com>
2026-04-20 12:58:54 -0500

    remove events

--- notification.tf
+++
@@ -1,6 +0,0 @@
-# this be where notifications are
-resource "oci_ons_notification_topic" "test_notification_topic" {
-    #Required
-    compartment_id = oci_identity_compartment.sandbox.id
-    name = "test_notification_id"
-}
