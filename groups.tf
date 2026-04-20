78f5403ffae9d60915fbaf810279fafcd7755a05
faarischaudhry <faaris.chaudhry@oracle.com>
2026-04-20 12:59:44 -0500

    Update groups.tf

--- groups.tf
+++ groups.tf
@@ -2,16 +2,10 @@ data "oci_identity_domain" "existing_domain" {
     domain_id = var.domain_id
 }
 
-resource "oci_identity_domains_group" "network_admins" {
+resource "oci_identity_domains_group" "sandbox_admins" {
     idcs_endpoint = data.oci_identity_domain.existing_domain.url
     schemas = ["urn:ietf:params:scim:schemas:core:2.0:Group"]
-    display_name = "NetworkAdmins"
-}
-
-resource "oci_identity_domains_group" "security_admins" {
-    idcs_endpoint = data.oci_identity_domain.existing_domain.url
-    schemas = ["urn:ietf:params:scim:schemas:core:2.0:Group"]
-    display_name = "SecurityAdmins"
+    display_name = "SandboxAdmins"
 }
 
 resource "oci_identity_domains_group" "groups" {
